5b0a3dbca804d2df2fcc6f23e2412ef37b423157
Mahdi Bouaziz <bouazizmedmahdi@gmail.com>
2025-02-02 13:21:35 +0100

    finished the cronjob controller

--- internal/controller/cronjob_controller.go
+++ internal/controller/cronjob_controller.go
@@ -18,8 +18,11 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"sort"
 	"time"
 
+	"github.com/robfig/cron"
 	kbatch "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -201,8 +204,225 @@ func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, err
 	}
 
-	return ctrl.Result{}, nil
+	/*
+		Once we've updated our status, we can move on to ensuring that the status of
+		the world matches what we want in our spec.
+
+		### 3: Clean up old jobs according to the history limit
+
+		First, we'll try to clean up old jobs, so that we don't leave too many lying
+		around.
+	*/
+
+	// NB: deleting these are "best effort" -- if we fail on a particular one,
+	// we won't requeue just to finish the deleting.
+	if cronJob.Spec.FailedJobsHistoryLimit != nil {
+		sort.Slice(failedJobs, func(i, j int) bool {
+			if failedJobs[i].Status.StartTime == nil {
+				return failedJobs[j].Status.StartTime != nil
+			}
+			return failedJobs[i].Status.StartTime.Before(failedJobs[j].Status.StartTime)
+		})
+		for i, job := range failedJobs {
+			if int32(i) >= int32(len(failedJobs))-*cronJob.Spec.FailedJobsHistoryLimit {
+				break
+			}
+
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); client.IgnoreNotFound(err) != nil {
+				log.Error(err, "unable to delete old failed job", "job", job)
+			} else {
+				log.V(0).Info("deleted old failed job", "job", job)
+			}
+		}
+	}
+
+	if cronJob.Spec.SuccessfulJobsHistoryLimit != nil {
+		sort.Slice(successfulJobs, func(i, j int) bool {
+			if successfulJobs[i].Status.StartTime == nil {
+				return successfulJobs[j].Status.StartTime != nil
+			}
+			return successfulJobs[i].Status.StartTime.Before(successfulJobs[j].Status.StartTime)
+		})
+		for i, job := range successfulJobs {
+			if int32(i) >= int32(len(successfulJobs))-*cronJob.Spec.SuccessfulJobsHistoryLimit {
+				break
+			}
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); err != nil {
+				log.Error(err, "unable to delete old successful job", "job", job)
+			} else {
+				log.V(0).Info("deleted old successful job", "job", job)
+			}
+		}
+	}
+
+	/* ### 4: Check if we're suspended
+
+	If this object is suspended, we don't want to run any jobs, so we'll stop now.
+	This is useful if something's broken with the job we're running and we want to
+	pause runs to investigate or putz with the cluster, without deleting the object.
+	*/
+	if cronJob.Spec.Suspend != nil && *cronJob.Spec.Suspend {
+		log.V(1).Info("cronjob suspended, skipping")
+		return ctrl.Result{}, nil
+	}
+
+	/*
+		### 5: Get the next scheduled run
+
+		If we're not paused, we'll need to calculate the next scheduled run, and whether
+		or not we've got a run that we haven't processed yet.
+	*/
+
+	// figure out the next times that we need to create
+	// jobs at (or anything we missed).
+	missedRun, nextRun, err := getNextSchedule(&cronJob, r.Now())
+	if err != nil {
+		log.Error(err, "unable to figure out CronJob schedule")
+		// we don't really care about requeuing until we get an update that
+		// fixes the schedule, so don't return an error
+		return ctrl.Result{}, nil
+	}
+
+	/*
+		We'll prep our eventual request to requeue until the next job, and then figure
+		out if we actually need to run.
+	*/
+	scheduledResult := ctrl.Result{RequeueAfter: nextRun.Sub(r.Now())} // save this so we can re-use it elsewhere
+	log = log.WithValues("now", r.Now(), "next run", nextRun)
+
+	/*
+		### 6: Run a new job if it's on schedule, not past the deadline, and not blocked by our concurrency policy
+
+		If we've missed a run, and we're still within the deadline to start it, we'll need to run a job.
+	*/
+	if missedRun.IsZero() {
+		log.V(1).Info("no upcoming scheduled times, sleeping until next")
+		return scheduledResult, nil
+	}
+
+	// make sure we're not too late to start the run
+	log = log.WithValues("current run", missedRun)
+	tooLate := false
+	if cronJob.Spec.StartingDeadlineSeconds != nil {
+		tooLate = missedRun.Add(time.Duration(*cronJob.Spec.StartingDeadlineSeconds) * time.Second).Before(r.Now())
+	}
+	if tooLate {
+		log.V(1).Info("missed starting deadline for last run, sleeping till next")
+		// TODO(directxman12): events
+		return scheduledResult, nil
+	}
+
+	/*
+		If we actually have to run a job, we'll need to either wait till existing ones finish,
+		replace the existing ones, or just add new ones.  If our information is out of date due
+		to cache delay, we'll get a requeue when we get up-to-date information.
+	*/
+	// figure out how to run this job -- concurrency policy might forbid us from running
+	// multiple at the same time...
+	if cronJob.Spec.ConcurrencyPolicy == batchv1.ForbidConcurrent && len(activeJobs) > 0 {
+		log.V(1).Info("concurrency policy blocks concurrent runs, skipping", "num active", len(activeJobs))
+		return scheduledResult, nil
+	}
+
+	// ...or instruct us to replace existing ones...
+	if cronJob.Spec.ConcurrencyPolicy == batchv1.ReplaceConcurrent {
+		for _, activeJob := range activeJobs {
+			// we don't care if the job was already deleted
+			if err := r.Delete(ctx, activeJob, client.PropagationPolicy(metav1.DeletePropagationBackground)); client.IgnoreNotFound(err) != nil {
+				log.Error(err, "unable to delete active job", "job", activeJob)
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	/*
+		Once we've figured out what to do with existing jobs, we'll actually create our desired job
+	*/
+
+	/*
+		We need to construct a job based on our CronJob's template.  We'll copy over the spec
+		from the template and copy some basic object meta.
+
+		Then, we'll set the "scheduled time" annotation so that we can reconstitute our
+		`LastScheduleTime` field each reconcile.
+
+		Finally, we'll need to set an owner reference.  This allows the Kubernetes garbage collector
+		to clean up jobs when we delete the CronJob, and allows controller-runtime to figure out
+		which cronjob needs to be reconciled when a given job changes (is added, deleted, completes, etc).
+	*/
+
+	constructJobForCronJob := func(cronJob *batchv1.CronJob, scheduledTime time.Time) (*kbatch.Job, error) {
+		// We want job names for a given nominal start time to have a deterministic name to avoid the same job being created twice
+		name := fmt.Sprintf("%s-%d", cronJob.Name, scheduledTime.Unix())
+
+		job := &kbatch.Job{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels:      make(map[string]string),
+				Annotations: make(map[string]string),
+				Name:        name,
+				Namespace:   cronJob.Namespace,
+			},
+			Spec: *cronJob.Spec.JobTemplate.Spec.DeepCopy(),
+		}
+
+		for k, v := range cronJob.Spec.JobTemplate.Annotations {
+			job.Annotations[k] = v
+		}
+		job.Annotations[scheduledTimeAnnotation] = scheduledTime.Format(time.RFC3339)
+
+		for k, v := range cronJob.Spec.JobTemplate.Labels {
+			job.Labels[k] = v
+		}
+
+		if err := ctrl.SetControllerReference(cronJob, job, r.Scheme); err != nil {
+			return nil, err
+		}
+
+		return job, nil
+	}
+
+	// actually make the job...
+	job, err := constructJobForCronJob(&cronJob, missedRun)
+	if err != nil {
+		log.Error(err, "unable to construct job from template")
+		// don't bother requeuing until we get a change to the spec
+		return scheduledResult, nil
+	}
+
+	// ...and create it on the cluster
+	if err := r.Create(ctx, job); err != nil {
+		log.Error(err, "unable to create Job for CronJob", "job", job)
+		return ctrl.Result{}, err
+	}
+
+	log.V(1).Info("created Job for CronJob run", "job", job)
+
+	/*
+		### 7: Requeue when we either see a running job or it's time for the next scheduled run
+
+		Finally, we'll return the result that we prepped above, that says we want to requeue
+		when our next run would need to occur.  This is taken as a maximum deadline -- if something
+		else changes in between, like our job starts or finishes, we get modified, etc, we might
+		reconcile again sooner.
+	*/
+	// we'll requeue once we see the running job, and update our status
+	return scheduledResult, nil
 }
+
+/*
+### Setup
+
+Finally, we'll update our setup.  In order to allow our reconciler to quickly
+look up Jobs by their owner, we'll need an index.  We declare an index key that
+we can later use with the client as a pseudo-field name, and then describe how to
+extract the indexed value from the Job object.  The indexer will automatically take
+care of namespaces for us, so we just have to extract the owner name if the Job has
+a CronJob owner.
+
+Additionally, we'll inform the manager that this controller owns some Jobs, so that it
+will automatically call Reconcile on the underlying CronJob when a Job changes, is
+deleted, etc.
+*/
 
 var (
 	jobOwnerKey = ".metadata.controller"
@@ -211,8 +431,32 @@ var (
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *CronJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// set up a real clock, since we're not in a test
+	if r.Clock == nil {
+		r.Clock = realClock{}
+	}
+
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &kbatch.Job{}, jobOwnerKey, func(rawObj client.Object) []string {
+		// grab the job object, extract the owner...
+		job := rawObj.(*kbatch.Job)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a CronJob...
+		if owner.APIVersion != apiGVStr || owner.Kind != "CronJob" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&batchv1.CronJob{}).
+		Owns(&kbatch.Job{}).
 		Named("cronjob").
 		Complete(r)
 }
@@ -220,11 +464,10 @@ func (r *CronJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
 //////////// Helpers section ////////////
 
 /*
-	We consider a job "finished" if it has a "Complete" or "Failed" condition marked as true.
-	Status conditions allow us to add extensible status information to our objects that other
-	humans and controllers can examine to check things like completion and health.
+We consider a job "finished" if it has a "Complete" or "Failed" condition marked as true.
+Status conditions allow us to add extensible status information to our objects that other
+humans and controllers can examine to check things like completion and health.
 */
-
 func isJobFinished(job *kbatch.Job) (bool, kbatch.JobConditionType) {
 	for _, c := range job.Status.Conditions {
 		if (c.Type == kbatch.JobComplete || c.Type == kbatch.JobFailed) && c.Status == corev1.ConditionTrue {
@@ -249,4 +492,54 @@ func getScheduledTimeForJob(job *kbatch.Job) (*time.Time, error) {
 		return nil, err
 	}
 	return &timeParsed, nil
+}
+
+/*
+We'll calculate the next scheduled time using our helpful cron library.
+We'll start calculating appropriate times from our last run, or the creation
+of the CronJob if we can't find a last run.
+
+If there are too many missed runs and we don't have any deadlines set, we'll
+bail so that we don't cause issues on controller restarts or wedges.
+
+Otherwise, we'll just return the missed runs (of which we'll just use the latest),
+and the next run, so that we can know when it's time to reconcile again.
+*/
+func getNextSchedule(cronJob *batchv1.CronJob, now time.Time) (lastMissed time.Time, next time.Time, err error) {
+	sched, err := cron.ParseStandard(cronJob.Spec.Schedule)
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("unparseable schedule %q: %v", cronJob.Spec.Schedule, err)
+	}
+
+	// for optimization purposes, cheat a bit and start from our last observed run time
+	// we could reconstitute this here, but there's not much point, since we've
+	// just updated it.
+	var earliestTime time.Time
+	if cronJob.Status.LastScheduleTime != nil {
+		earliestTime = cronJob.Status.LastScheduleTime.Time
+	} else {
+		earliestTime = cronJob.ObjectMeta.CreationTimestamp.Time
+	}
+	if cronJob.Spec.StartingDeadlineSeconds != nil {
+		// controller is not going to schedule anything below this point
+		schedulingDeadline := now.Add(-time.Second * time.Duration(*cronJob.Spec.StartingDeadlineSeconds))
+		if schedulingDeadline.After(earliestTime) {
+			earliestTime = schedulingDeadline
+		}
+	}
+	if earliestTime.After(now) {
+		return time.Time{}, sched.Next(now), nil
+	}
+
+	starts := 0
+	for t := sched.Next(earliestTime); !t.After(now); t = sched.Next(t) {
+		lastMissed = t
+
+		starts++
+		if starts > 100 {
+			// We can't get the most recent times so just return an empty slice
+			return time.Time{}, time.Time{}, fmt.Errorf("too many missed start times (> 100). Set or decrease .spec.startingDeadlineSeconds or check clock skew")
+		}
+	}
+	return lastMissed, sched.Next(now), nil
 }
